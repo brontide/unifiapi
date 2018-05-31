759075a13c5f51a453a02d79142cda13c7d6b80b
Eric Warnke <ericew@gmail.com>
2018-05-30 23:09:11 -0400

    Clear pylint warnings

--- sync_backups.py
+++ sync_backups.py
@@ -19,8 +19,8 @@ print("Getting backup listing")
 backups = s.c_backups()
 
 for backup in backups:
-    full_file = dest_dir / Path(backup['filename'])
-    if full_file.exists():
+    full_file = Path(dest_dir / Path(backup['filename']))
+    if full_file.is_file():
         stat = full_file.stat()
         if stat.st_size != int(backup['size']):
             print(f"{full_file} DOES NOT MATCH FILESIZE")
