34b60952aa8b12955d9530273396924076048428
Eric Warnke <ericew@gmail.com>
2019-11-04 08:08:50 -0500

    Tools for extracting DPI constants

---
+++ extract_dpi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+import json
+import sys
+import re
+
+data = open(sys.argv[1]).read()
+#print(data)
+
+applications = {}
+for app in re.findall('\d+:{.*?}',re.search('applications:{(.*?)}}',data).group(1)):
+    foo = re.search('(.+):{name:\"(.*?)\"',app)
+    #print(app)
+    applications[foo.group(1)] = foo.group(2)
+    
+categories = {}
+for cat in re.findall('\d+:{.*?}',re.search('categories:{(.*?})}',data).group(1)):
+    foo = re.search('(.+):{name:\"(.*?)\"',cat)
+    #print(cat)
+    categories[foo.group(1)] = foo.group(2)
+    
+print(json.dumps({"categories":categories,"applications":applications}))
